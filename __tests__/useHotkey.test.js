918a0314f0402ffdff2a5b789371150026557d59
Geoff Davis <geoffdavis92@gmail.com>
2019-02-23 17:44:27 -0600

    removed unused codeblocks

--- __tests__/useHotkey.test.js
+++ __tests__/useHotkey.test.js
@@ -65,21 +65,10 @@ describe("Test useHotkey", () => {
   it("Should fire onClick", () => {
     const mockedReturnsArgs = jest.fn(returnsArg);
     const component = mount(<HotkeyTest fn={() => mockedReturnsArgs(b + 1)} />);
-    const keydownEvents = new KeydownLibrary(["a", "b"]);
     const getButton = () => component.find("button");
 
     getButton().simulate("click");
     expect(mockedReturnsArgs).toBeCalledTimes(1);
     expect(b).toBe(1);
-
-    // window.addEventListener("keydown", ({ key }) => console.log({ key }));
-
-    // act(() => {
-    //   window.dispatchEvent(new KeyboardEvent("keydown", { key: "a" }));
-    // });
-    // act(() => {
-    //   window.dispatchEvent(new KeyboardEvent("keydown", { key: "b" }));
-    // });
-    // expect(mockedReturnsArgs).toBeCalledTimes(2);
   });
 });
